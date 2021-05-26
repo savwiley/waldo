89ce5cf44a1fe4913a0523a38a0e1d7a71920a5c
Savannaha <savwiley@yahoo.com>
2021-05-26 15:01:05 -0500

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default Header;
+
+/**
+ * I need this to stick to the top of the page and not be very tall.
+ * 
+ * I'm thinking chars to find on left side, timer on right side (powered by firebase), and MAYBE the app's title in the middle if there's room on mobile.
+ * 
+ * Maybe connect the 'add high score' logic in here.
+ * 
+ * If there are different difficulties, they should be noted in here so that the images of chars can change.
+ */
