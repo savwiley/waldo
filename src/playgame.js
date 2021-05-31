982f90633bd178b1b27fa2f997ad257dd4b16436
Savannaha <savwiley@yahoo.com>
2021-05-31 13:41:35 -0500

    removes dropdown when clicked on image & displays win msg

--- src/playgame.js
+++ src/playgame.js
@@ -8,14 +8,24 @@ const PlayGame = () => {
     const objs = Array.from(document.querySelectorAll(".object"));
     const imageMap = document.querySelector(".image");
     const dropdown = document.querySelector(".dropdown");
+    dropdown.style.display = "none";
+    const correctScreen = document.querySelector(".correct");
     let objectContent;
+
+    const correctTime = () => {
+      correctScreen.style.display = "none"
+    }
 
     //makes dropdown appear where clicked
     imageMap.addEventListener("click", (e) => {
-      dropdown.style.display = "block";
-      dropdown.style.left = e.pageX + "px";
-      dropdown.style.top = e.pageY + "px";
-      objectContent = "imageMap";
+      if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+        dropdown.style.left = e.pageX + "px";
+        dropdown.style.top = e.pageY + "px";
+        objectContent = "imageMap";
+      } else {
+        dropdown.style.display = "none";
+      }
     })
 
     //adds the object id's to dropdown list
@@ -23,6 +33,7 @@ const PlayGame = () => {
       //creates elements in list
       const section = document.createElement("span");
       section.setAttribute("class", "section");
+      section.setAttribute("id", `${e.alt}`);
       section.style.display = "block";
       section.textContent = e.id;
       //adds dropdown after clicking on chars
@@ -30,7 +41,7 @@ const PlayGame = () => {
         dropdown.style.display = "block";
         dropdown.style.left = a.pageX + "px";
         dropdown.style.top = a.pageY + "px";
-        objectContent = e.id;
+        objectContent = e.alt;
       });
       return dropdown.appendChild(section);
     })
@@ -38,17 +49,19 @@ const PlayGame = () => {
     //selects correct/wrong answer from dropdown
     const section = Array.from(document.querySelectorAll(".section"));
     dropdown.addEventListener("click", (a) => {
-      if (a.path[0].innerText === objectContent) {
+      const item = document.querySelector(`area[alt=${objectContent}]`);
+      if (a.path[0].id === objectContent) {
         //RIGHT logic here
         console.log("CORRECT");
-        const item = document.querySelector(`#${objectContent}`);
+        correctScreen.style.display = "block";
         item.remove();
         section.map(e => {
-          if (e.textContent === objectContent) {
+          if (e.id === objectContent) {
             e.remove();
           }
           return e;
         })
+        setTimeout(correctTime(), 5 * 1000);
       } else {
         console.log("WRONG");
         //WRONG logic here
@@ -71,12 +84,14 @@ const PlayGame = () => {
       <area alt="neo" id="Neo" className="object" coords="728,313,849,403" shape="rect" />
       <area alt="minion" id="Minion" className="object" coords="1795,1587,1846,1652" shape="rect" />
       <area alt="ash" id="Ash Ketchum" className="object" coords="27,2530,87,2581" shape="rect" />
-      <area alt="jabba" id="Jabba the Hut" className="object" coords="1169,1017,1254,1133" shape="rect" />
+      <area alt="jabba" id="Jabba the Hutt" className="object" coords="1169,1017,1254,1133" shape="rect" />
       <area alt="edward" id="Edward Elric" className="object" coords="1099,2846,1182,2906" shape="rect" />
       <area alt="link" id="Link" className="object" coords="371,3188,476,3285" shape="rect" /> 
     </map>
 
     <div className="dropdown"></div>
+
+    <div className="correct">CORRECT</div>
     </>
   );
 };
@@ -89,5 +104,4 @@ export default PlayGame;
   https://imagemap.org/
 
   create right/wrong logic
-  maybe make the dropdown vanish when you click off of it, too
   */
