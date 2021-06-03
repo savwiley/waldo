37790a45e22d685d871173e7558ac8f672da4c7e
Savannaha <savwiley@yahoo.com>
2021-06-03 16:19:25 -0500

    ran prettier

--- src/playgame.js
+++ src/playgame.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import Header from "./components/Header.js";
 
 const PlayGame = () => {
-
   useEffect(() => {
     //selectors
     const objs = Array.from(document.querySelectorAll(".object"));
@@ -19,18 +18,18 @@ const PlayGame = () => {
         dropdown.style.display = "block";
         let X;
         let Y;
-        e.pageX > 1700 ? X = e.pageX - 200 : X = e.pageX;
-        e.pageY > 3350 ? Y = e.pageY - 300 : Y = e.pageY;
+        e.pageX > 1700 ? (X = e.pageX - 200) : (X = e.pageX);
+        e.pageY > 3350 ? (Y = e.pageY - 300) : (Y = e.pageY);
         dropdown.style.left = X + "px";
         dropdown.style.top = Y + "px";
         objectContent = "imageMap";
       } else {
         dropdown.style.display = "none";
       }
-    })
+    });
 
     //adds the object id's to dropdown list
-    objs.map(e => {
+    objs.map((e) => {
       //creates elements in list
       const section = document.createElement("span");
       section.setAttribute("class", "section");
@@ -41,22 +40,22 @@ const PlayGame = () => {
       e.addEventListener("click", (a) => {
         dropdown.style.display = "block";
         let X;
-        a.pageX > 1700 ? X = a.pageX - 200 : X = a.pageX;
+        a.pageX > 1700 ? (X = a.pageX - 200) : (X = a.pageX);
         dropdown.style.left = X + "px";
         dropdown.style.top = a.pageY + "px";
         objectContent = e.alt;
       });
       return dropdown.appendChild(section);
-    })
+    });
 
     //removes correctScreen after 3 seconds
     const correctTime = () => {
-      correctScreen.style.display = "none"
-    }
+      correctScreen.style.display = "none";
+    };
     //removes wrongScreen after 3 seconds
     const wrongTime = () => {
-      wrongScreen.style.display = "none"
-    }
+      wrongScreen.style.display = "none";
+    };
 
     //selects correct/wrong answer from dropdown
     const section = Array.from(document.querySelectorAll(".section"));
@@ -65,44 +64,102 @@ const PlayGame = () => {
       if (a.path[0].id === objectContent) {
         correctScreen.style.display = "block";
         item.remove();
-        section.map(e => {
+        section.map((e) => {
           if (e.id === objectContent) {
             e.remove();
           }
           return e;
-        })
+        });
         setTimeout(correctTime, 2000);
       } else {
         wrongScreen.style.display = "block";
         setTimeout(wrongTime, 2000);
       }
       dropdown.style.display = "none";
-    })
-    
+    });
   });
 
-  return(
+  return (
     <>
-    <Header />
+      <Header />
 
-    <img src="https://raw.githubusercontent.com/savwiley/waldo/master/src/images/egor-klyuchnyk-kickstarter-artstation.jpg" className="image" useMap="#image" alt="2222AD" />
+      <img
+        src="https://raw.githubusercontent.com/savwiley/waldo/master/src/images/egor-klyuchnyk-kickstarter-artstation.jpg"
+        className="image"
+        useMap="#image"
+        alt="2222AD"
+      />
 
-    <map name="image" className="imageMap">
-      <area alt="sonic" id="Sonic the Hedgehog" className="object" coords="609,1866,684,1926" shape="rect" />
-      <area alt="portal" id="Frankenturret" className="object" coords="1286,1334,1354,1422" shape="rect" />
-      <area alt="bravo" id="Johnny Bravo" className="object" coords="781,1810,903,1924" shape="rect" />
-      <area alt="neo" id="Neo" className="object" coords="728,313,849,403" shape="rect" />
-      <area alt="minion" id="Minion" className="object" coords="1795,1587,1846,1652" shape="rect" />
-      <area alt="ash" id="Ash Ketchum" className="object" coords="27,2530,87,2581" shape="rect" />
-      <area alt="jabba" id="Jabba the Hutt" className="object" coords="1169,1017,1254,1133" shape="rect" />
-      <area alt="edward" id="Edward Elric" className="object" coords="1099,2846,1182,2906" shape="rect" />
-      <area alt="link" id="Link" className="object" coords="371,3188,476,3285" shape="rect" /> 
-    </map>
+      <map name="image" className="imageMap">
+        <area
+          alt="sonic"
+          id="Sonic the Hedgehog"
+          className="object"
+          coords="609,1866,684,1926"
+          shape="rect"
+        />
+        <area
+          alt="portal"
+          id="Frankenturret"
+          className="object"
+          coords="1286,1334,1354,1422"
+          shape="rect"
+        />
+        <area
+          alt="bravo"
+          id="Johnny Bravo"
+          className="object"
+          coords="781,1810,903,1924"
+          shape="rect"
+        />
+        <area
+          alt="neo"
+          id="Neo"
+          className="object"
+          coords="728,313,849,403"
+          shape="rect"
+        />
+        <area
+          alt="minion"
+          id="Minion"
+          className="object"
+          coords="1795,1587,1846,1652"
+          shape="rect"
+        />
+        <area
+          alt="ash"
+          id="Ash Ketchum"
+          className="object"
+          coords="27,2530,87,2581"
+          shape="rect"
+        />
+        <area
+          alt="jabba"
+          id="Jabba the Hutt"
+          className="object"
+          coords="1169,1017,1254,1133"
+          shape="rect"
+        />
+        <area
+          alt="edward"
+          id="Edward Elric"
+          className="object"
+          coords="1099,2846,1182,2906"
+          shape="rect"
+        />
+        <area
+          alt="link"
+          id="Link"
+          className="object"
+          coords="371,3188,476,3285"
+          shape="rect"
+        />
+      </map>
 
-    <div className="dropdown"></div>
+      <div className="dropdown"></div>
 
-    <div className="correct">CORRECT</div>
-    <div className="wrong">WRONG</div>
+      <div className="correct">CORRECT</div>
+      <div className="wrong">WRONG</div>
     </>
   );
 };
@@ -113,4 +170,13 @@ export default PlayGame;
 
 /** 
   https://imagemap.org/
+
+  Now hook up the functionality for validating with your back end whether the user has clicked in the right place for that character.
+
+  >>> OH DEAR I accidentally made the whole game in react.
+  >>> https://firebase.google.com/codelabs/firebase-web#15
+  >>> there's also React Firebase which is a thing we could use
+  >>> https://react-firebase-js.com/
+
+  Tie it into your front end so you can seamlessly select characters, validate them, and place the appropriate markers on the map if the selection was correct.
   */
