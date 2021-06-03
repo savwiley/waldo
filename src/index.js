bd54c9247624b797bfa9e2c1dcceea3de858e153
Savannaha <savwiley@yahoo.com>
2021-06-03 16:18:57 -0500

    moved firebase funcs to own file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import "./style/style.css";
-import firebase from "firebase/app";
-import "firebase/analytics";
-import "firebase/firestore";
-import "firebase/auth";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,15 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAkjNiDhl58ZnU8FxJSeMenlm08snE9JOA",
-  authDomain: "waldo-9e96c.firebaseapp.com",
-  projectId: "waldo-9e96c",
-  storageBucket: "waldo-9e96c.appspot.com",
-  messagingSenderId: "605827275032",
-  appId: "1:605827275032:web:4118ece57107b7db5c266d",
-  measurementId: "G-X9XEP41LEE"
-};
-
-firebase.initializeApp(firebaseConfig);
