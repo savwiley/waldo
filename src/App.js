606df9fcef7dfa8387f5d25ee2fbd917b0b3c7cd
Savannaha <savwiley@yahoo.com>
2021-06-05 15:26:21 -0500

    created difficulties

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 //import Header from "./components/Header.js";
 
 function App() {
-  
+  //try useParams from react router
+  const refDifficulty = useRef();
+
+  //BE SURE to figure out how to make the player select a difficulty before they go to the game
+
+  useEffect(() => {
+    //selectors
+    const easyBtn = document.querySelector("#easy");
+    const medBtn = document.querySelector("#medium");
+    const hardBtn = document.querySelector("#hard");
+
+    easyBtn.addEventListener("click", () => {
+      refDifficulty.current = "easy";
+    })
+    medBtn.addEventListener("click", () => {
+      refDifficulty.current = "medium";
+    })
+    hardBtn.addEventListener("click", () => {
+      refDifficulty.current = "hard";
+    })
+  });
+
+
   return(
     <>
-      <Link to="/game">
-        Game!
-      </Link>
+      <div className="start">
+
+        <div id="easy" className="diff">
+          Easy
+        </div>
+
+        <div id="medium" className="diff">
+          Medium
+        </div>
+
+        <div id="hard" className="diff">
+          Hard
+        </div>
+
+        <Link to="/game">
+          Game!
+        </Link>
+
+      </div>
     </>
   )
 }
