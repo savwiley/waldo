cd8e2e79a6cc837832856276ef8153dee9a40939
Savannaha <savwiley@yahoo.com>
2021-06-06 19:04:41 -0500

    added images

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,25 +12,61 @@ const Header = (props) => {
   }
 
   useEffect(() => {
-    const header = document.querySelector(".header");
+    //selectors
+    const first = document.querySelector("#one");
+    const firstImage = document.querySelector(".imageOne");
+    const second = document.querySelector("#two");
+    const secondImage = document.querySelector(".imageTwo");
+    const third = document.querySelector("#three");
+    const thirdImage = document.querySelector(".imageThree");
+
+    //const header = document.querySelector(".header");
     if (!docs) {
       callAsync();
     } else {
       //v holds the data like a json
       let headText = [];
       docs.forEach(e => {
-        headText.push(e.data().id);
+        headText.push(e.data());
       })
-      //v change this to make it look better with images and things
       //also, consider starting the timer at this point
-      header.textContent = headText;
+      first.textContent = headText[0].id;
+      firstImage.style.background = `url("https://raw.githubusercontent.com/savwiley/waldo/master/src/images/${headText[0].image}")`;
+      firstImage.style.backgroundPosition = "center";
+      firstImage.style.backgroundRepeat = "no-repeat";
+      firstImage.style.backgroundSize = "contain";
+
+      second.textContent = headText[1].id;
+      secondImage.style.background = `url("https://raw.githubusercontent.com/savwiley/waldo/master/src/images/${headText[1].image}")`;
+      secondImage.style.backgroundPosition = "center";
+      secondImage.style.backgroundRepeat = "no-repeat";
+      secondImage.style.backgroundSize = "contain";
+
+      third.textContent = headText[2].id;
+      thirdImage.style.background = `url("https://raw.githubusercontent.com/savwiley/waldo/master/src/images/${headText[2].image}")`;
+      thirdImage.style.backgroundPosition = "center";
+      thirdImage.style.backgroundRepeat = "no-repeat";
+      thirdImage.style.backgroundSize = "contain";
     }
   });
 
   return (
     <div className="header">
-      
-      Loading...
+
+      <div className="imageOne"></div>
+      <div id="one">
+        Loading...
+      </div>
+
+      <div className="imageTwo"></div>
+      <div id="two">
+        Loading...
+      </div>
+
+      <div className="imageThree"></div>
+      <div id="three">
+        Loading...
+      </div>
 
     </div>
   )
