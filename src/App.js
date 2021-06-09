192fdad6b7e98ea86ef1db1a3a130d331e57cfca
Savannaha <savwiley@yahoo.com>
2021-06-09 14:30:04 -0500

    edited comments

--- src/App.js
+++ src/App.js
@@ -26,6 +26,12 @@ function App() {
           </div>
         </Link>
 
+        <Link to="scores/EasyScores">
+          <div>
+            to scores
+          </div>
+        </Link>
+
       </div>
     </>
   )
@@ -37,14 +43,17 @@ export default App;
 
 /**
  * TODOS
- * - add images to header
- * - create timer
  * - make a better correct/wrong screen
  * - create a You Found Them All or whatever screen
  * -- figure out how to decide this happened, when dropdown is empty?
  * -- give player ability to add high score after stopping timer
  * - create high score table (set with different difficulties)
  * - create links to high score table (in App.js and Header.js?)
+ * - fix dropdown so it doesn't overflow off screen instead of just off page
+ * - clean up CSS
+ * - mobile friendly
+ * - consider creating an error page if async can't be called, likely because of INTERNET PROBLEMS
  * - edit README
  * - create LICENSE
+ * - fix tab pic/name
  */
