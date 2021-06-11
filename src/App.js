5b79fbbd818df3c7fbcb686f5c878b891200e4a2
Savannaha <savwiley@yahoo.com>
2021-06-11 12:29:05 -0500

    added images, high score link, & gh link

--- src/App.js
+++ src/App.js
@@ -1,38 +1,76 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 //import Header from "./components/Header.js";
 
 function App() {
 
+  useEffect(() => {
+    //arrays
+    const easy = ["jabba.png", "neo.png", "portal.png"];
+    const medium = ["bravo.gif", "edward.png", "link.png"];
+    const hard = ["ash.png", "minion.png", "sonic.png"];
+
+    //random number generator
+    const randomNumb = () => {
+      return Math.floor(Math.random() * 3);
+    };
+
+    //selectors
+    const easyImage = document.querySelector("#easyImage");
+    const medImage = document.querySelector("#medImage");
+    const hardImage = document.querySelector("#hardImage");
+
+    //backgrounds
+    easyImage.style.background = "center / contain no-repeat url('https://raw.githubusercontent.com/savwiley/waldo/master/src/images/" + easy[randomNumb()] + "')";
+    medImage.style.background = "center / contain no-repeat url('https://raw.githubusercontent.com/savwiley/waldo/master/src/images/" + medium[randomNumb()] + "')";
+    hardImage.style.background = "center / contain no-repeat url('https://raw.githubusercontent.com/savwiley/waldo/master/src/images/" + hard[randomNumb()] + "')";
+
+  })
+
   return(
     <>
       <div className="start">
 
-        <Link to="easy">
-          <div id="easy" className="diff">
-            Easy
-          </div>
-        </Link>
+        <div className="appFlex">
 
-        <Link to="medium">
-          <div id="medium" className="diff">
-            Medium
-          </div>
-        </Link>
+          <div className="homeImage" id="easyImage"></div>
 
-        <Link to="hard">
-          <div id="hard" className="diff">
-            Hard
-          </div>
-        </Link>
+          <Link to="easy">
+            <div id="easy" className="diff">
+              Easy
+            </div>
+          </Link>
+
+          <div className="homeImage" id="medImage"></div>
+
+          <Link to="medium">
+            <div id="medium" className="diff">
+              Medium
+            </div>
+          </Link>
+
+          <div className="homeImage" id="hardImage"></div>
+
+          <Link to="hard">
+            <div id="hard" className="diff">
+              Hard
+            </div>
+          </Link>
+
+        </div>
 
         <Link to="scores/EasyScores">
-          <div>
-            to scores
+          <div id="scoresLink" className="diff">
+            High Scores
           </div>
         </Link>
 
       </div>
+
+      <div className="button">
+        <a href="https://github.com/savwiley/waldo">GitHub</a>
+      </div>
+
     </>
   )
 }
@@ -47,8 +85,6 @@ export default App;
  * - create a You Found Them All or whatever screen
  * -- figure out how to decide this happened, when dropdown is empty?
  * -- give player ability to add high score after stopping timer
- * - create high score table (set with different difficulties)
- * - create links to high score table (in App.js and Header.js?)
  * - fix dropdown so it doesn't overflow off screen instead of just off page
  * - clean up CSS
  * - mobile friendly
