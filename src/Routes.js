0dba36d3812ebd8d4bc0e3c1e4668e4b19695222
Savannaha <savwiley@yahoo.com>
2021-06-13 14:00:44 -0500

    added About route

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 import App from "./App.js";
 import PlayGame from "./playgame.js";
 import HighScore from "./highscore.js";
+import About from "./about.js";
 
 const Routes = () => {
 
@@ -10,6 +11,7 @@ const Routes = () => {
     <HashRouter basename="/">
       <Switch>
         <Route exact path="/" render={() => <App />} />
+        <Route exact path="/about" render={() => <About />} />
         <Route exact path="/:difficulty" render={() => <PlayGame />} />
         <Route exact path="/scores/:diffScore" render={() => <HighScore />} />
       </Switch>
