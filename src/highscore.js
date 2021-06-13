b7464c8d754d5f5c6adf9cd362330ec2017b5021
Savannaha <savwiley@yahoo.com>
2021-06-13 14:01:54 -0500

    added logic to mark most recent score & created headers

--- src/highscore.js
+++ src/highscore.js
@@ -4,30 +4,41 @@ import firebase from "./firebase.js";
 
 const HighScore = () => {
   const [ docs, setDocs ] = useState();
+  const [ recent, setRecent ] = useState();
   const { diffScore } = useParams();
   const newDiff = useRef();
 
-  /**
-   * collections are EasyScores, MedScores, HardScores
-   * docs are the names of the users
-   * the only field is "score" with the time in a string
-   */
+   //collections are EasyScores, MedScores, HardScores
 
   async function callAsync() {
     const doc = firebase.firestore().collection(diffScore).orderBy('score');
-    newDiff.current = diffScore;
+    const rec = firebase.firestore().collection(diffScore).orderBy('date', 'desc').limit(1);
     const docGet = await doc.get();
+    const recGet = await rec.get();
+    newDiff.current = diffScore;
     setDocs(docGet);
+    setRecent(recGet);
   };
 
   useEffect(() => {
     //selectors
     const table = document.querySelector(".table");
     const sections = Array.from(document.querySelectorAll(".sect"));
+    const scoresTitle = document.querySelector(".scoresTitle");
 
-    if (!docs || newDiff.current !== diffScore) {
+    //gives each page a title
+    if (diffScore === "EasyScores") {
+      scoresTitle.textContent = "High Scores - Easy";
+    } else if (diffScore === "MedScores") {
+      scoresTitle.textContent = "High Scores - Medium";
+    } else if (diffScore === "HardScores") {
+      scoresTitle.textContent = "High Scores - Hard";
+    }
+
+    if (!docs || !recent || newDiff.current !== diffScore) {
       table.textContent = "Loading...";
       callAsync();
+      //removes sections of other difficulties
       if (sections) {
         sections.forEach(e => {
           e.remove();
@@ -35,21 +46,38 @@ const HighScore = () => {
         });
       };
     } else {
+      //holds data
       let name = [];
       let score = [];
+      let mostRecent;
       table.textContent = "";
+      //gathers data
       docs.forEach(e => {
-        name.push(e.id);
+        name.push(e.data().id);
         score.push(e.data().score);
       });
+      recent.forEach(e => {
+        mostRecent = e.data();
+      });
+
       for (let i = 0; i < name.length; i++) {
+        //creates each section
         const sect = document.createElement("div");
           sect.setAttribute("class", "sect");
           table.appendChild(sect);
+        //name & placement number
         const sectName = document.createElement("div");
           sectName.setAttribute("id", "sectName");
           sectName.textContent = `${i+1}. ${name[i]}`;
           sect.appendChild(sectName);
+        //marks most recent score
+        const sectRecent = document.createElement("div");
+          sectRecent.setAttribute("id", "sectRecent");
+          if (mostRecent.id === name[i]) {
+            sectRecent.textContent = "🟊 most recent";
+          };
+          sect.appendChild(sectRecent);
+        //score/time
         const sectTime = document.createElement("div");
           sectTime.setAttribute("id", "sectTime");
           sectTime.textContent = score[i];
@@ -67,6 +95,8 @@ const HighScore = () => {
         <Link to="HardScores">Hard</Link>
       </div>
 
+      <div className="scoresTitle"></div>
+
       <div className="table">Loading...</div>
 
       <div className="button">
@@ -77,19 +107,3 @@ const HighScore = () => {
 }
 
 export default HighScore;
-
-/**
- * a "table" with names & scores
- * use "tabs" at the top to switch to different difficulties (maybe with #easy, etc)
- * always open on easy tab unless brought here from other difficulty
- * pull this stuff directly from firebase
- * btn that goes back to App.js
- * 
- * 
- * 
-      {scoring.map((e) => (
-        <div>
-          {e.score}
-        </div>
-      ))}
- */
